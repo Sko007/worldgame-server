3c54adf0a19cd8d43a38ebc03c19c1b5a0e1da4c
Severin <severin_koch@gmx.de>
2020-02-11 14:43:59 +0100

    portnumber

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT
+const port = process.env.PORT || 4000
 const bodyParser = require("body-parser");
 const jsonParser = bodyParser.json();
 const authRouter = require("./auth/router");
